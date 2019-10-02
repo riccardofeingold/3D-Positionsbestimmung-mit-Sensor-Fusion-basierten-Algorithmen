bdd78831648d51081611378900fb4709114ed5cf
Riccardo <riccardofeingold@gmail.com>
2019-10-02 11:23:18 +0200

    2.10.19

--- Texte/gps.cpp
+++ Texte/gps.cpp
@@ -1,8 +1,5 @@
-GPS.begin(9600);
-GPS.sendCommand(PMTK_ENABLE_SBAS);//Enable search for SBAS satellite (works only with 1Hz output rate)
-GPS.sendCommand(PMTK_ENABLE_WAAS);//Enable DGPS to correct the postion data
-GPS.sendCommand(PMTK_SET_NMEA_OUTPUT_RMCGGA);//Means nothing else than: print only RMC (lon,lat) and GGA (height over sealevel) data => both have the necessary data
-GPS.sendCommand(PMTK_SET_NMEA_UPDATE_1HZ);//Updating frequence 
-GPS.sendCommand(PGCMD_ANTENNA);//I assume this says the GPS-shield that an antenna is attached 
-delay(1000);
-mySerial.println(PMTK_Q_RELEASE);
+typedef enum {international=0, dwd, gnss, map_matching, comparing} State_type; //This enumeration contains all the main states
+void internationaleFormel(), DWD(), gps(), MAP_MATCHING(), COMPARE();
+void (*state_table[])() = {internationaleFormel, DWD, gps, MAP_MATCHING, COMPARE}; //This table conatins a pointer to the function to call in each state
+State_type curr_state;
+State_type prev_state;
