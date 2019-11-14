2a17f0dca6965338fe37533d3ec959622de1be81
Riccardo <riccardofeingold@gmail.com>
2019-11-14 19:41:53 +0100

    14.11.19

---
+++ SwissMapDaten/DatenSortierer/SortData.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+
+float lat, lon, height;
+float lat_max, lat_min, lon_max, lon_min;
+
+int main(void)
+{
+    FILE *f = fopen("DatasetOfCH.txt","r");
+    FILE *g = fopen("dataset.txt","w");
+    
+    //ask for users input data
+    printf("Geben Sie die Koordinate (lat/lon) der oberen linken Ecke und von Ihrer ausgewählten Umgebung auf Swissmap (siehe Kap. 6.2.4): ");
+    scanf("%f, %f",&lat_max, &lon_min);
+
+    printf("Geben Sie die Koordinate (lat/lon) der unteren rechten Ecke von Ihrer ausgewählten Umgebung auf Swissmap: ");
+    scanf("%f, %f",&lat_min, &lon_max);
+    
+    if (f == NULL){
+        printf("Could not open file f");
+        return 1;
+    }
+    
+    if (g==NULL){
+        printf("Could not open file g");
+        return 1;
+    }
+    
+    while (!feof(f))
+    {
+        fscanf(f,"%f,%f,%f,\n",&lon, &lat, &height);
+        
+        if (lat <= lat_max && lat >= lat_min)
+        {
+            if (lon <= lon_max && lon >= lon_min)
+            {
+                fprintf(g,"%f,%f,%f,\n",lat,lon,height);
+            }
+        }
+    }
+    fclose(f);
+    fclose(g),
+    printf("Finished!");
+    return 0;
+}
